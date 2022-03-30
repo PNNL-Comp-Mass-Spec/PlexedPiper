468b0322245dfa8e6811259d0af4c1040d9eada3
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2022-03-30 15:56:09 -0700

    Update test-remap_accessions_uniprot_to_gene_fasta.R
    Uncomment and update test to use the new "uniprot_rat_small.fasta.gz" file in MSnID.

--- tests/testthat/test-remap_accessions_uniprot_to_gene_fasta.R
+++ tests/testthat/test-remap_accessions_uniprot_to_gene_fasta.R
@@ -1,28 +1,30 @@
-# test_that("remap_uniprot_to_gene_fasta works", {
-#   path_to_FASTA <- system.file("extdata", "uniprot_rat_small.fasta",
-#                                package = "MSnID")
-#   temp_dir <- tempdir()
-#   file.copy(path_to_FASTA, temp_dir)
-#   path_to_FASTA <- file.path(temp_dir, basename(path_to_FASTA))
-#
-#   # Create new FASTA file and output file path
-#   path_to_FASTA_gene <- remap_accessions_uniprot_to_gene_fasta(
-#     path_to_FASTA
-#   )
-#
-#   fst_gene <- readAAStringSet(path_to_FASTA_gene)
-#
-#   expect_equal(width(fst_gene[c("A1bg", "A1cf", "A1i3", "A1m", "A2m",
-#                                 "A3galt2", "A4galt", "Aacs",
-#                                 "Aadac", "Aadat")]),
-#                c(513, 594, 1477, 1500, 1472, 339, 360, 672, 398, 425))
-#   expect_equal(length(fst_gene), 7986)
-#
-#   # Clean up
-#   temp_files <- list.files(temp_dir, full.names = TRUE)
-#   temp_files <- temp_files[!grepl("\\.fasta$", temp_files)]
-#   file.remove(temp_files)
-#   unlink(".Rcache", recursive = TRUE)
-#   unlink("../../.Rcache", recursive = TRUE)
-# })
+test_that("remap_uniprot_to_gene_fasta works", {
+  path_to_FASTA <- system.file("extdata", "uniprot_rat_small.fasta.gz",
+                               package = "MSnID")
+  temp_dir <- tempdir()
+  file.copy(path_to_FASTA, temp_dir)
+  path_to_FASTA <- file.path(temp_dir, basename(path_to_FASTA))
+
+  # Create new FASTA file and output file path
+  path_to_FASTA_gene <- remap_accessions_uniprot_to_gene_fasta(
+    path_to_FASTA
+  )
+
+  fst_gene <- readAAStringSet(path_to_FASTA_gene)
+
+  expect_equal(width(fst_gene[c("Aatf", "Acsl4", "Alkbh5", "Andpro",
+                                "Angpt1", "Atp2b2", "B4galt1", "Calml3",
+                                "Ccnf", "Cct3")]),
+               c(523, 711, 395, 176, 497, 1205, 32, 149, 780, 545))
+  expect_equal(length(fst_gene), 97)
+
+  # Clean up
+  try(expr = {
+    temp_files <- list.files(temp_dir, full.names = TRUE)
+    temp_files <- temp_files[!grepl("\\.fasta$", temp_files)]
+    file.remove(temp_files)
+    unlink(".Rcache", recursive = TRUE)
+    unlink("../../.Rcache", recursive = TRUE)
+  })
+})
 
