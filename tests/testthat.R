696fc103551fbf516e4a822aa337a50e02eb4998
TylerSagendorf <tjsagendorf@gmail.com>
2023-07-06 08:44:12 -0700

    Update tests
    Update test-remap_accessions_refseq_to_gene_fasta to use specific conversion table to ensure reproducibility.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
 library(PlexedPiper)
+library(PlexedPiperTestData)
 
 test_check("PlexedPiper")
--- tests/testthat/test-remap_accessions_refseq_to_gene_fasta.R
+++ tests/testthat/test-remap_accessions_refseq_to_gene_fasta.R
@@ -1,5 +1,11 @@
 test_that("remap_accessions_refseq_to_gene_fasta works", {
   library(Biostrings)
+  library(MSnID)
+
+  conv_tbl <- fetch_conversion_table(
+    organism_name = "Rattus norvegicus",
+    from = "REFSEQ", to = "SYMBOL",
+    snapshot_date = "2023-04-24")
 
   path_to_FASTA <- system.file(
     "extdata/Rattus_norvegicus_NCBI_RefSeq_2018-04-10.fasta.gz",
@@ -10,15 +16,16 @@ test_that("remap_accessions_refseq_to_gene_fasta works", {
   path_to_FASTA <- file.path(temp_dir, basename(path_to_FASTA))
   suppressMessages(
     path_to_new_FASTA <- remap_accessions_refseq_to_gene_fasta(
-      path_to_FASTA, organism_name = "Rattus norvegicus"
-    )
+      path_to_FASTA, organism_name = "Rattus norvegicus",
+      conversion_table = conv_tbl)
   )
   fst <- readAAStringSet(path_to_new_FASTA) # gene symbols
 
   expect_equal(head(names(fst)),
-               c("A1bg", "A1cf", "A2m", "A3galt2", "A4galt", "A4gnt"))
+               c("1700013D24Rik", "A1bg", "A1cf",
+                 "A2m", "A3galt2", "A4galt"))
   expect_equal(head(width(fst)),
-               c(513, 594, 1472, 339, 360, 351))
+               c(120, 513, 594, 1472, 339, 360))
 
   # Clean up
   temp_files <- list.files(temp_dir, full.names = TRUE)
